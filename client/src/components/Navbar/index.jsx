e5bc8d091b37e6d9eb6ee5b8f0c2a7cc1f8803a4
0l3h <kubik7950@gmail.com>
2022-07-03 21:42:04 +0300

    Added some changes to navigation bar

--- client/src/components/Navbar/index.jsx
+++ client/src/components/Navbar/index.jsx
@@ -1,29 +1,54 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
 import styles from './Navbar.module.sass';
+import UserDropdown from '../UserDropdown';
 
 function Navbar() {
+  const [isOpen, setIsOpen] = useState(false); 
+  const [isLogged, setIsLogged] = useState(true);
+
+  const toggleMenu = e => {
+    e.target.style = { 
+      backgroundImage: 'url(../../images.close-menu-icon.svg)' 
+    };
+    
+    setIsOpen(isOpen => !isOpen);
+  };
+
   return <nav className={styles.navbar}>
     <img src="/logo.png" alt="logo" className={styles.logo}/>
-    <NavLink className={styles.navLink} to='/'>
-      Home
-    </NavLink>
     
-    <NavLink className={styles.navLink} to='/browse-questions'>
-      Questions
-    </NavLink>
-    
-    <NavLink className={styles.navLink} to='/ask-question'>
-      Ask a question
-    </NavLink>
-    
-    <NavLink className={styles.loginButton} to='/log-in'>
-      Log in
-    </NavLink>
-    
-    <NavLink className={styles.signupButton} to='/sign-up'>
-      Sign up
-    </NavLink>
+    <button className={styles.menuToggle} onClick={toggleMenu} data-menu-open={isOpen.toString()}>
+    </button>
+
+    <section className={styles.links}>
+      <NavLink className={styles.navLink} to='/'>
+        Home
+      </NavLink>
+      
+      <NavLink className={styles.navLink} to='/browse-questions'>
+        Questions
+      </NavLink>
+      
+      <NavLink className={styles.navLink} to='/ask-question'>
+        Ask a question
+      </NavLink>
+
+      {
+        isLogged? 
+          <UserDropdown/>
+          :
+          <>
+            <NavLink className={styles.loginButton} to='/log-in'>
+              Log in
+            </NavLink>
+            
+            <NavLink className={styles.signupButton} to='/sign-up'>
+              Sign up
+            </NavLink>
+          </>
+      }
+    </section>
   </nav>;
 }
 
