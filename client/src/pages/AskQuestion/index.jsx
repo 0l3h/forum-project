1c2a50222979787262b6c6b579902ba84dd114da
0l3h <kubik7950@gmail.com>
2022-07-13 13:17:25 +0300

    Added formik to ask question page Changed background styles

--- client/src/pages/AskQuestion/index.jsx
+++ client/src/pages/AskQuestion/index.jsx
@@ -1,33 +1,47 @@
 import React from 'react';
-import warningIcon from '../../images/warning-icon.png'
+import { Formik, Form } from 'formik';
+import { useDispatch } from 'react-redux';
+import { askQuestion } from '../../slices/questionsSlice.js';
+import Input from '../../components/Input';
+import warningIcon from '../../images/warning-icon.png';
 import Page from '../Page';
 import TextInput from '../../components/TextInput';
 import styles from './AskQuestion.module.sass';
+import schemas from '../../validation/validationSchema';
 
 function AskQuestion() {
+    const dispatch = useDispatch();
+    
+    const initialValues = {
+        title: '',
+        questionBody: ''
+    };
+
+    const submit = values => {
+        dispatch(askQuestion(values));
+    };
 
     return (
         <Page>
             <main className={styles.content}>
-                <section className={styles.warning}>
-                    <img src={warningIcon} alt="warning icon" />
-                    <p>When writing a question, remember to follow our community rules</p>
-                </section>
+                <Formik initialValues={initialValues} validationSchema={schemas.questionSchema} onSubmit={submit}>
+                    <Form className={styles.questionForm}>
+                        <h1 className={styles.heading}>Ask a public question</h1>
+                        
+                        <section className={styles.warning}>
+                            <img src={warningIcon} className={styles.warningIcon} alt="warning icon" />
+                            <p>When writing a question, remember to follow our community rules</p>
+                        </section>
 
-                <form className={styles.questionForm}>
-                    <h1 className={styles.heading}>Ask a public question</h1>
+                        <Input type="text" label="Title" name="title" className={styles.titleInput}/>
 
-                    <label htmlFor="title">Title</label>
-                    <input type="text" name="title" placeholder="Title"/>
+                        <TextInput name="questionBody"/>
 
-                    <label htmlFor="question">Question</label>
-                    <TextInput type="text" name="question"/>
-
-                    <button type="submit" className={styles.postButton}>
-                        Post
-                    </button>
-                </form>
-
+                        <button type="submit" className={styles.postButton}>
+                            Post
+                        </button>
+                    </Form>
+                </Formik>
             </main>
         </Page>
     )
