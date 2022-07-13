5a586a4824530f68c863388531e483077d7f4cc0
0l3h <kubik7950@gmail.com>
2022-07-13 22:54:28 +0300

    Modified existing routes, added nested ones

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,5 @@
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Page from "./pages/Page";
 import Home from './pages/Home';
 import BrowseQuestions from './pages/BrowseQuestions';
 import NotFound from './pages/NotFound';
@@ -8,21 +9,53 @@ import AskQuestion from './pages/AskQuestion';
 import Question from './pages/Question';
 import LogIn from './pages/LogIn';
 import SignUp from './pages/SignUp';
+import AuthRequired from "./components/AuthRequired";
+import NotAuthorized from "./components/NotAuthorized";
 
 function App() {
   return (
     <div>
       <Router>
         <Routes>
-          <Route path='/' element={<Home/>}/>
-          <Route path='/browse-questions' element={<BrowseQuestions/>}/>
-          <Route path='/log-in' element={<LogIn/>}/>
-          <Route path='/sign-up' element={<SignUp/>}/>
-          <Route path='/ask-question' element={<AskQuestion/>}/>
-          <Route path='/edit-profile' element={<EditProfile/>}/>
-          <Route path='/question' element={<Question/>}/>
-          <Route path='/view-profile' element={<ViewProfile/>}/>
-          <Route path='*' element={<NotFound/>}/>
+          <Route element={<Page/>}>
+            <Route index element={<Home/>}/>
+
+            <Route path='/browse-questions' element={<BrowseQuestions/>}>
+              <Route path=':questionId' element={<Question/>}/>
+            </Route>
+
+            <Route path='/log-in' element={
+              <NotAuthorized>
+                <LogIn/>
+              </NotAuthorized>
+            }/>
+
+            <Route path='/sign-up' element={
+              <NotAuthorized>
+                <SignUp/>
+              </NotAuthorized>
+            }/>
+
+            <Route path='/ask-question' element={
+              <AuthRequired>
+                <AskQuestion/>
+              </AuthRequired>
+            }/>
+
+            <Route path='/edit-profile' element={
+              <AuthRequired>
+                <EditProfile/>
+              </AuthRequired>
+            }/>
+
+            <Route path='/view-profile' element={
+              <AuthRequired>
+                <ViewProfile/>
+              </AuthRequired>
+            }/>
+
+            <Route path='*' element={<NotFound/>}/>
+          </Route>
         </Routes>
       </Router>
     </div>
