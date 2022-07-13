dfec4575aa1099dcb000a8267643d9dff4bf433e
0l3h <kubik7950@gmail.com>
2022-07-13 06:36:25 +0300

    Added navlinks to footer component Changed some styles

--- client/src/components/Footer/index.jsx
+++ client/src/components/Footer/index.jsx
@@ -1,17 +1,28 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 import styles from './Footer.module.sass';
 import twitterLogo from '../../images/twitter-logo.png';
 import linkedinLogo from '../../images/linkedin-logo.png';
 import instagramLogo from '../../images/instagram-logo.png';
 
 function Footer() {
-    return <footer className="footer">
-        <section className="follow-us">
-            <span>Follow us</span>
-            <img className="media-logo" src={twitterLogo} alt="Twitter logo" />
-            <img className="media-logo" src={linkedinLogo} alt="LinkedIn logo" />
-            <img className="media-logo" src={instagramLogo} alt="Instagram logo" />
+    return <footer className={styles.footer}>
+        <NavLink to="/credits" className={styles.credits}>Credits</NavLink>
+
+        <section className={styles.media}>
+            <NavLink to="/credits" className={styles.credits}>
+                <img className={styles.mediaLogo} src={twitterLogo} alt="Twitter"/>
+            </NavLink>
+
+            <NavLink to="/credits" className={styles.credits}>
+                <img className={styles.mediaLogo} src={linkedinLogo} alt="LinkedIn"/>
+            </NavLink>
+            
+            <NavLink to="/credits" className={styles.credits}>
+                <img className={styles.mediaLogo} src={instagramLogo} alt="Instagram"/>
+            </NavLink>
         </section>
+
         <span>2022 Ⓒ All rights reserved</span>
     </footer>
 }
