996e65bd868ebeb36d4b4a55adcf8a1dbb323abe
0l3h <kubik7950@gmail.com>
2022-07-19 20:18:31 +0300

    Added saga middleware to the store

--- client/src/store/store.js
+++ client/src/store/store.js
@@ -1,8 +1,17 @@
 import { configureStore } from '@reduxjs/toolkit';
+import createSagaMiddleware from '@redux-saga/core';
 import questionsReducer from '../slices/questionsSlice.js';
+import rootSaga from '../sagas/index.js';
+
+const sagaMiddleware = createSagaMiddleware();
 
 export default configureStore({
     reducer: {
         questions: questionsReducer,
     },
+    middleware: getDefaultMiddleware => {
+        return getDefaultMiddleware().concat(sagaMiddleware)
+    },
 });
+
+sagaMiddleware.run(rootSaga);
