b6e57e3bfe2543188402fe84241903f8f9c02d77
0l3h <kubik7950@gmail.com>
2022-07-19 20:14:43 +0300

    Added new redux actions in question slice

--- client/src/slices/questionsSlice.js
+++ client/src/slices/questionsSlice.js
@@ -1,19 +1,37 @@
+import { v4 as uuidv4 } from 'uuid';
 import { createSlice } from '@reduxjs/toolkit';
 
 export const questionsSlice = createSlice({
     name: 'questions',
     initialState: {
         questionPosts: [],
+        isFetching: false,
+        error: null,
     },
     reducers: {
-        askQuestion: (state, action) => {
+        createQuestionRequest: (state) => {
+            state.isFetching = true;
+            state.error = null;
+        },
+        createQuestionSuccess: (state, action) => {
             const { questionPosts } = state;
             const { title, questionBody } = action.payload;
 
-            questionPosts.push({id: Math.random(), title, questionBody });
+            console.log(action.payload);
+
+            questionPosts.push({id: uuidv4(), title, questionBody });
+        },
+        createQuestionError: (state, action) => {
+            const { error } = action.payload;
+            
+            state.error = error;
+            state.isFetching = false;
         }
     }
-})
+});
 
-export const { askQuestion } = questionsSlice.actions;
+export const { createQuestionRequest } = questionsSlice.actions;
+export const { createQuestionSuccess } = questionsSlice.actions;
+export const { createQuestionError } = questionsSlice.actions;
+
 export default questionsSlice.reducer;
