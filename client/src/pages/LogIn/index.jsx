26d778eb37275211506f1ac6a225afc967178c49
0l3h <kubik7950@gmail.com>
2022-09-05 21:16:03 +0300

    Added page redirect to Login page Defined data retrieving logic

--- client/src/pages/LogIn/index.jsx
+++ client/src/pages/LogIn/index.jsx
@@ -1,7 +1,12 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { NavLink, useNavigate, useLocation } from 'react-router-dom';
 import { Formik, Form } from 'formik';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import companyLogo from '../../images/logo.png';
+import loginImage from '../../images/login-illustration.svg';
+import instagramLogo from '../../images/instagram-logo.png';
+import twitterLogo from '../../images/twitter-logo.png';
+import linkedInLogo from '../../images/linkedin-logo.png';
 import { logInRequest } from '../../slices/auth.slice';
 import schemas from '../../validation/validationSchema';
 import styles from './LogIn.module.sass';
@@ -12,6 +17,10 @@ function LogIn() {
     const navigate = useNavigate();
     const location = useLocation();
 
+    const selector = state => state.auth.userData.id; 
+
+    const userId = useSelector(selector);
+
     const from = location.state?.from?.pathname || "/";
 
     const initialValues = {
@@ -19,19 +28,43 @@ function LogIn() {
         password: '',
     }
 
+    useEffect(() => {
+        console.log(userId, from)
+        userId && navigate(from, { replace: true });
+    });
+
     const submit = values => {
         dispatch(logInRequest(values));
-
-        navigate(from, { replace: true });
     }
 
     return (
         <main className={styles.content}>
+            <div className={styles.sideSection}>
+        
+                <h1>Welcome!</h1>
+    
+                <p>Enter the system to use Askme fully</p>
+
+                {/* <img src={loginImage} alt="" /> */}
+
+                <div className={styles.decor}></div>
+
+                <section className={styles.socials}>
+                    <img src={instagramLogo} alt="" />
+                    <img src={twitterLogo} alt="" />
+                    <img src={linkedInLogo} alt="" />
+                </section>
+
+                <div className={styles.decor2}></div>
+            </div>
+            
             <Formik 
                 initialValues={initialValues} 
                 validationSchema={schemas.loginSchema}
                 onSubmit={submit}>
                 <Form className={styles.loginForm}>
+                    <img src={companyLogo} alt="logo" />
+
                     <h1 className={styles.heading}>Log in</h1>
 
                     <Input type="text" label="Username" name="username" placeholder='Username'/>
