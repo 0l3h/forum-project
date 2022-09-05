a6f8e48f552d1e7f358020e828d398ac26f2b8cf
0l3h <kubik7950@gmail.com>
2022-09-05 21:13:18 +0300

    Created Auth component wrapper Defined component logic and page redirect

--- client/src/components/AuthRequired/index.jsx
+++ client/src/components/AuthRequired/index.jsx
@@ -1,10 +1,18 @@
-import React from 'react';
-import { Navigate } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { Navigate, useLocation } from 'react-router-dom';
+import { getMyProfileRequest } from '../../slices/auth.slice';
 
 function AuthRequired({ children }) {
-    const user = undefined;
+  const location = useLocation();
+  const dispatch = useDispatch();
+  const userId = useSelector(({ auth: { userData } }) => userData.id);
 
-    return user? children : <Navigate to="/log-in"/>;
+  useEffect(() => {
+    dispatch(getMyProfileRequest());
+  });
+
+  return userId? children : <Navigate to="/log-in" state={{ from: location }} replace/>;
 }
 
 export default AuthRequired;
