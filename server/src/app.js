d4d0b3bcaa4b570a91ed19dede4d1fdf9bc2edf9
0l3h <kubik7950@gmail.com>
2022-10-28 18:37:28 +0300

    Changes

--- server/src/app.js
+++ server/src/app.js
@@ -35,7 +35,7 @@ app.post('/log-in', login);
 
 if(process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, '../../client/build')));
-    // app.get('/*', (req, res, next) => { res.sendFile(path.join(__dirname, '../../client/build/index.html')) });
+    app.get('/*', (req, res, next) => { res.sendFile(path.join(__dirname, '../../client/build/index.html')) });
 }
 
 app.listen(port, () => {
