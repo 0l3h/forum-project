dfbcb111f296b8ac9a4ae797f91446a22c353b8a
0l3h <kubik7950@gmail.com>
2022-10-30 16:12:32 +0200

    Fixed sign up page

--- client/src/pages/SignUp/index.jsx
+++ client/src/pages/SignUp/index.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { NavLink, useNavigate, useLocation } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 import { signUpRequest } from '../../slices/user.slice';
