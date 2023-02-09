db7e7b1b68e6b0e7721109e2ea0a6c0be6084572
0l3h <kubik7950@gmail.com>
2023-02-09 23:43:11 +0200

    Modify api end-points

--- server/src/app.js
+++ server/src/app.js
@@ -5,10 +5,30 @@ const express = require('express');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
 
-const { getQuestions, createQuestion, getQuestionById, getPopularQuestions, voteQuestion } = require('./controllers/questions.controller');
-const { createAnswer, voteAnswer } = require('./controllers/answers.controller'); 
+const { 
+    getQuestions, 
+    createQuestion, 
+    getQuestionById, 
+    getPopularQuestions, 
+    voteQuestion, 
+    deleteQuestionVote } = require('./controllers/questions.controller');
+
+const { 
+    createAnswer, 
+    voteAnswer,
+    deleteAnswerVote
+} = require('./controllers/answers.controller'); 
+
 const { getUsers } = require('./controllers/user.controller');
-const { signup, login, auth, getMyProfile, updateMyProfile, logout } = require('./controllers/auth.controller');
+
+const { 
+    signup, 
+    login, 
+    auth, 
+    getMyProfile, 
+    updateMyProfile, 
+    logout 
+} = require('./controllers/auth.controller');
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -19,7 +39,9 @@ app.use(cors({
     credentials: true, 
     origin: process.env.NODE_ENV === 'production'? 'https://forum-project-production-bca8.up.railway.app/' : 'http://localhost:3000'
 }));
-app.use(express.json({limit: "10mb"}));
+
+app.use(express.json({ limit: "10mb" }));
+
 app.use(cookieParser());
 
 if(process.env.NODE_ENV === 'production')
@@ -30,14 +52,19 @@ app.get('/browse-questions/:id', getQuestionById);
 app.get('/view-profile', auth, getMyProfile);
 app.get('/users', getUsers);
 app.get('/popular-questions', getPopularQuestions);
-app.post('/browse-questions/:id', auth, createAnswer);
+
 app.patch('/browse-questions/:id/vote-question', auth, voteQuestion);
-app.patch('/browse-questions/:id/vote-answer', auth, voteAnswer);
-app.post('/ask-question', auth, createQuestion);
+app.patch('/browse-questions/*/vote-answer', auth, voteAnswer);
 app.patch('/edit-profile', auth, updateMyProfile);
+
+app.post('/ask-question', auth, createQuestion);
+app.post('/browse-questions/:id', auth, createAnswer);
 app.post('/sign-up', signup);
 app.post('/log-in', login);
 app.post('/log-out', logout);
+
+app.delete('/browse-questions/:id/delete-question-vote', auth, deleteQuestionVote);
+app.delete('/browse-questions/*/delete-answer-vote', auth, deleteAnswerVote);
 
 app.listen(port, () => {
     console.log(`\nServer is listening on port: ${port}\n`);
