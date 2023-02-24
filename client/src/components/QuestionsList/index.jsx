3df888b9abbdefe7c353c8b261da7bd85958ac77
0l3h <kubik7950@gmail.com>
2023-02-24 23:48:57 +0200

    Add loading spinner to questions list

--- client/src/components/QuestionsList/index.jsx
+++ client/src/components/QuestionsList/index.jsx
@@ -3,10 +3,12 @@ import { v4 as uuidv4 } from 'uuid';
 import { useSelector, useDispatch } from 'react-redux';
 import { getQuestionsRequest } from '../../slices/questions.slice';
 import Question from '../../components/Question';
+import LoadingSpinner from './../LoadingSpinner';
 import styles from './QuestionsList.module.sass';
 
 function QuestionsList() {
     const questions = useSelector(state => state.questions.questionPosts);
+    const isFetching = useSelector(state => state.questions.isFetching);
     
     const dispatch = useDispatch();
 
@@ -14,21 +16,24 @@ function QuestionsList() {
         dispatch(getQuestionsRequest());
     }, []);
 
-    return <>
-        <section className={styles.questions}>
-            <h1 className={styles.heading}>All questions</h1>
+    if(isFetching) {
+        return <LoadingSpinner/>
+    } else {
+        return (
+            <section className={styles.questions}>
+                <h1 className={styles.heading}>All questions</h1>
 
-            {
-                questions.length? 
-                questions.map(q => {
-                    return <Question key={uuidv4()} id={q.id} question={q}/>;
-                })
-                :
-                <div>No questions have been asked yet</div>
-            }
-        </section>
-    </>
-        
+                {
+                    questions.length? 
+                    questions.map(q => {
+                        return <Question key={uuidv4()} id={q.id} question={q}/>;
+                    })
+                    :
+                    <div>No questions have been asked yet</div>
+                }
+            </section>
+        )
+    }    
 };
 
 export default QuestionsList;
